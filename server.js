72bd8a657cb71d504915caa33a6e35f4abc1c863
simiatracer <simia.tracer@gmail.com>
2022-04-26 17:49:27 +0200

    bozza pagina prenotazioni
    le modifiche a pagine esterne sono state fatte solo per linkare

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get("/chisiamo", (req,res) => {
     res.sendFile(path.join(__dirname, "static/templates/chiSiamo.html"));
 });
 
+app.get("/prenota", (req,res) => {
+    res.sendFile(path.join(__dirname, "static/templates/prenota.html"));
+});
+
 app.listen(8000, () => {
     console.log("Server is running on port 8000");
 });
